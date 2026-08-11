6b67231049203e33d9cd8ef2ba9532bdeaaaecce
AgungDevlop <142754535+AgungDevlop@users.noreply.github.com>
2026-08-12 02:21:17 +0500

    Update PlayVideo.tsx

--- src/pages/PlayVideo.tsx
+++ src/pages/PlayVideo.tsx
@@ -19,7 +19,7 @@ interface VideoData {
 
 const ADS = [
   'https://dulyhagglermounting.com/2082665',
-  'https://sorrowfulpsychology.com/HE9TFh'
+  'https://bmadss.com/get/?spot_id=2026313&cat=25&subid=1576043020'
   ];
 
 export function PlayVideo() {
